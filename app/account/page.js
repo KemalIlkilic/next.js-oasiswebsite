e260bec542e0920726829ffc0ade5c2ff4124cbc
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-01-28 17:30:24 +0300

    Implement navigation component and layout

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>Hello Cabins</h1>
+      <h1>Hello Account</h1>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
+import Link from 'next/link';
 
 export default function Page() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+      <Link href="/cabins">Explore Luxury Cabins</Link>
+    </div>
+  );
 }
