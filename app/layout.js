c5eb71b5ceeb9862fcde7420d1c5fd0668fc2897
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-01-29 17:25:11 +0300

    Add logo component, data service functions, global styles, and layout updates

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,33 @@
-import Navigation from './components/Navigation';
+import Logo from './_components/Logo';
+import Navigation from './_components/Navigation';
+import { Josefin_Sans } from 'next/font/google';
+import '@/app/_styles/globals.css';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const metadata = {
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description: 'Luxurious cabin hotel',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Navigation />
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
         <main>{children}</main>
+        <footer>Copyright</footer>
       </body>
     </html>
   );
